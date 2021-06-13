d40ae5d75beae9cc8b1cd302b07b7209340e019e
Muhammad Ali <muhammad.ali0094@outlook.com>
2021-06-13 11:37:52 +0400

    initial commit

---
+++ basicactivity/android/app/src/main/kotlin/com/example/basicactivity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basicactivity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ basicactivity/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(child: Column(
+
+          ))
+        ],
+      ),
+    )
+
+    return MaterialApp(
+        title: 'Flutter Layout',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.teal,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Layout learning'),
+          ),
+          body: Center(
+            child: Text(
+              'Hi there!',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
